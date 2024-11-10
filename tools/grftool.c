b0e0493dfaa12ebd9d04cb2d0278a9c9ffb40654
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2024-11-10 11:06:20 +0900

    [clean] Refactoring

--- tools/grftool.c
+++ tools/grftool.c
@@ -21,83 +21,88 @@
 #include <string.h>
 #include "grf.h"
 
-
-static void
-usage (int e)
+static void usage(int e)
 {
-	printf ("Usage: grftool <GRF-FILE> [FILENAME] [--ends]\n\n"
-		"Arguments:\n"
-		"  GRF-FILE    If only this is given, a list of all files inside this GRF file\n"
-		"              will be printed.\n"
-		"  FILENAME    If this argument is given too, then then this file (inside the\n"
-		"              GRF archive) will be extracted and printed.\n"
-		"  --ends      If given, the file whose name ends with FILENAME will be\n"
-		"              extracted and printed.\n");
-	exit (e);
+    printf("Usage: grftool <GRF-FILE> [FILENAME] [--ends]\n\n"
+           "Arguments:\n"
+           "  GRF-FILE    If only this is given, a list of all files inside this GRF file\n"
+           "              will be printed.\n"
+           "  FILENAME    If this argument is given too, then then this file (inside the\n"
+           "              GRF archive) will be extracted and printed.\n"
+           "  --ends      If given, the file whose name ends with FILENAME will be\n"
+           "              extracted and printed.\n");
+    exit(e);
 }
 
-
-static int
-str_has_suffix (const char *str, const char *suffix)
+static int str_has_suffix(const char *str, const char *suffix)
 {
-	int len1, len2;
+    int len1, len2;
 
-	len1 = strlen (str);
-	len2 = strlen (suffix);
-	if (len1 < len2) return 0;
-	return (strcmp (str + len1 - len2, suffix) == 0);
+    len1 = strlen(str);
+    len2 = strlen(suffix);
+    if (len1 < len2)
+        return 0;
+    return (strcmp(str + len1 - len2, suffix) == 0);
 }
 
-
-int
-main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	Grf *grf;
-	GrfError error;
+    Grf *grf;
+    GrfError error;
 
-	if (!argv[1])
-		usage (2);
+    if (!argv[1])
+        usage(2);
 
-	grf = grf_open (argv[1], "rb", &error);
-	if (!grf) {
-		fprintf (stderr, "Error: %s\n", grf_strerror (error));
-		return 1;
-	}
+    grf = grf_open(argv[1], "rb", &error);
+    if (!grf)
+    {
+        fprintf(stderr, "Error: %s\n", grf_strerror(error));
+        return 1;
+    }
 
-	if (argv[2]) {
-		uint32_t i;
-		char *file = NULL;
-		void *data;
+    if (argv[2])
+    {
+        uint32_t i;
+        char *file = NULL;
+        void *data;
 
-		if (argv[3] && strcmp (argv[3], "--ends") == 0) {
-			for (i = 0; i < grf->nfiles; i++) {
-				if (str_has_suffix (grf->files[i].name, argv[2])) {
-					file = grf->files[i].name;
-					break;
-				}
-			}
-			if (!file) {
-				fprintf (stderr, "Error: file not found\n");
-				return 1;
-			}
-		} else
-			file = argv[2];
+        if (argv[3] && strcmp(argv[3], "--ends") == 0)
+        {
+            for (i = 0; i < grf->nfiles; i++)
+            {
+                if (str_has_suffix(grf->files[i].name, argv[2]))
+                {
+                    file = grf->files[i].name;
+                    break;
+                }
+            }
+            if (!file)
+            {
+                fprintf(stderr, "Error: file not found\n");
+                return 1;
+            }
+        }
+        else
+            file = argv[2];
 
-		data = grf_get (grf, file, &i, &error);
-		if (data)
-			fwrite (data, 1, i, stdout);
-		else {
-			fprintf (stderr, "Error extracting %s: %s\n", file, grf_strerror (error));
-			return 1;
-		}
+        data = grf_get(grf, file, &i, &error);
+        if (data)
+            fwrite(data, 1, i, stdout);
+        else
+        {
+            fprintf(stderr, "Error extracting %s: %s\n", file, grf_strerror(error));
+            return 1;
+        }
+    }
+    else
+    {
+        unsigned long i;
+        for (i = 0; i < grf->nfiles; i++)
+        {
+            printf("%s\n", grf->files[i].name);
+        }
+    }
 
-	} else {
-		unsigned long i;
-		for (i = 0; i < grf->nfiles; i++) {
-			printf ("%s\n", grf->files[i].name);
-		}
-	}
-
-	grf_free (grf);
-	return 0;
+    grf_free(grf);
+    return 0;
 }
